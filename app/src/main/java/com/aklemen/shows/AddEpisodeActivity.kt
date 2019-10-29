65207a5b4657c2d69b97fe6c81b9b913f09c906b
Anton Klemen <antonklemen@gmail.com>
2019-10-29 21:50:53 +0100

    Login screen layout fixed

--- app/src/main/java/com/aklemen/shows/AddEpisodeActivity.kt
+++ app/src/main/java/com/aklemen/shows/AddEpisodeActivity.kt
@@ -1,12 +1,17 @@
 package com.aklemen.shows
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
 import kotlinx.android.synthetic.main.activity_add_episode.*
 
@@ -16,6 +21,7 @@ class AddEpisodeActivity : AppCompatActivity() {
 
         const val EXTRA_ADD_TITLE = "AddEpisodeActivity.addTitle"
         const val EXTRA_ADD_DESCRIPTION = "AddEpisodeActivity.addDescription"
+        const val REQUEST_CAMERA = 222
 
         fun newStartIntent(context: Context): Intent {
             return Intent(context, AddEpisodeActivity::class.java)
@@ -44,6 +50,32 @@ class AddEpisodeActivity : AppCompatActivity() {
 
         addEditTitle.doOnTextChanged { text, _, _, _ -> setSaveButtonState(text.toString(), addEditDescription) }
         addEditDescription.doOnTextChanged { text, _, _, _ -> setSaveButtonState(text.toString(), addEditTitle) }
+
+        addImageCamera.setOnClickListener {
+            openCamera()
+        }
+        addTextUploadImage.setOnClickListener {
+            openCamera()
+        }
+    }
+
+    private fun openCamera() {
+        //TODO Check permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                AlertDialog.Builder(this)
+                    .setMessage("We just need your permission so you can take a photo of you.")
+                    .setPositiveButton("Ok") { _, _ ->
+                    }
+                    .create()
+                    .show()
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA)
+            }
+        }
+        startActivityForResult(Intent(MediaStore.ACTION_IMAGE_CAPTURE), REQUEST_CAMERA)
     }
 
     // Alert on back button
